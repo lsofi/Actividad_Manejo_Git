85f7c2921162e7e53f9f6c481f0d339ed9b2dc9e
Joaquin-N <j.neschisi@gmail.com>
2020-08-05 10:59:41 -0300

    Comentario en usuario

--- MiPrimerProyecto/MiPrimerProyecto/frmLogin.cs
+++ MiPrimerProyecto/MiPrimerProyecto/frmLogin.cs
@@ -12,7 +12,7 @@ namespace MiPrimerProyecto
 {
     public partial class frmLogin : Form
     {
-        string user = "adm";
+        string user = "adm"; // Usuario de administrador
         string pass = "123456";
         public frmLogin()
         {
